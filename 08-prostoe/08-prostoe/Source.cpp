55174b6cbc5dd25b61c93ea664f5445625ba85cf
VerzunovaKsenia <verzunova.02@mail.ru>
2021-09-16 18:45:37 +0300

    08-prostoe

---
+++ 08-prostoe/08-prostoe/Source.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <ctime>
+int main()
+{
+    float N;
+    int b;
+    int k = 0;
+    float eps = 0.00001;
+    std::cin >> N;
+    b = int(N);
+    float h;
+    h = float(b);
+    if ((b <= 0)  or ((N - h) > eps))
+    {
+        std::cout<<"ошибка\n";
+    }
+    else
+    {
+        for (int i = 2; i <= b/2; i++)
+        {
+            if (b % i == 0)
+            {
+                k = k + 1;
+                break;
+            }
+        }   
+        if (k > 0)
+        {
+            std::cout<<"составное\n";
+        }
+        else
+        {
+            std::cout<<"простое\n";
+        }
+    }
+    //std::cout << "runtime = " << clock()/1000.0 ;
+    return 0;
+}
