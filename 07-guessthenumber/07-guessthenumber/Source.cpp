c2e4e8065699d65f4f7cdb7dd207c3c3c233da74
VerzunovaKsenia <verzunova.02@mail.ru>
2021-09-27 10:56:38 +0300

    07-guessthenumber

--- 07-guessthenumber/07-guessthenumber/Source.cpp
+++ 07-guessthenumber/07-guessthenumber/Source.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+
 int main() {
     int i; 
     int N;
+	int k = 0;
     int a = 1;
     int b = 100;
-    std::cout << "Задумайте число N от 1 до 100, а я угадаю!\n";
-    std::cout << "N больше 50 (1 да, 2 нет, 3 равно) ?\n";
+    std::cout << "Zadumaite chislo ot 1 do 100!\n";
+    std::cout << "N bolshe 50 (1 yes, 2 no, 3 equal) ?\n";
     std::cin >> i;
     while (i != 3)
     {
@@ -13,17 +15,20 @@ int main() {
         {
             a = (a + b) / 2;
             N = (a + b) / 2;
-            std::cout << "N больше" << N << "(1 да, 2 нет, 3 равно) ? \n";
+            std::cout << "N bolshe " << N << " (1 yes, 2 no, 3 equal) ? \n";
             std::cin >> i;
         }
         else
         {
             b = (a + b) / 2;
             N = (a + b) / 2;
-            std::cout << "N больше" << N << "(1 да, 2 нет, 3 равно) ? \n";
+            std::cout << "N bolshe " << N << " (1 yes, 2 no, 3 equal) ? \n";
             std::cin >> i;
         }
+		k = k + 1;
     }
-    std::cout << N ;
+    std::cout << N <<"\n" ;
+    std::cout << k <<"\n" ;
+	system("pause");
     return 0;
 }
