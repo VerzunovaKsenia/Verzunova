b18d63660038ffa21c365a0c4810ff9e4b9585ab
VerzunovaKsenia <90266692+VerzunovaKsenia@users.noreply.github.com>
2021-10-28 20:55:36 +0300

    19-LIFOFIFO

--- main.cpp
+++ main.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-#include "Crect.h"
+#include "FIFO.h"
 
 int main()
 {
-	using namespace std;
-
-	ifstream f("coordinates.txt");
-	int xl1, yl1, xr1, yr1, xl2, yl2, xr2, yr2;
-    f >> xl1 >> yl1 >> xr1>> yr1 >> xl2 >> yl2 >> xr2 >> yr2;
-	Crect rect1(xl1, yl1, xr1, yr1), rect2(xl2, yl2, xr2, yr2);
-	rect1.cross(rect2);
+	int N;
+	cout<<"VVedite colichestvo chisel: ";
+	cin >> N;
+	FIFO mas(N);
+	mas.push();
+	mas.pop();
 	system("pause");
-	return 0;
+    return 0;
 }
