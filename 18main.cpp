02357d4c34a6d66a2e9d199bfb3bb552772c6dc5
VerzunovaKsenia <90266692+VerzunovaKsenia@users.noreply.github.com>
2021-10-28 20:58:50 +0300

    18main.cpp

--- 18main.cpp
+++ 18main.cpp
@@ -9,7 +9,7 @@ int main()
 
 	ifstream f("coordinates.txt");
 	int xl1, yl1, xr1, yr1, xl2, yl2, xr2, yr2;
-    f >> xl1 >> yl1 >> xr1>> yr1 >> xl2 >> yl2 >> xr2 >> yr2;
+        f >> xl1 >> yl1 >> xr1>> yr1 >> xl2 >> yl2 >> xr2 >> yr2;
 	Crect rect1(xl1, yl1, xr1, yr1), rect2(xl2, yl2, xr2, yr2);
 	rect1.cross(rect2);
 	system("pause");
